75a38bd197c164203eb1e43faa543fbf3096d9b0
area6080 <161906317+area6080@users.noreply.github.com>
2024-04-10 05:58:49 +0000

    [Create]sidebar2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,40 +5,46 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def show
     @user = User.find(params[:id])
-    @book = Book.find(params[:user_id])
+    @book = @user.books.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
-    @book = Book.find(params[:user_id])
+    @book = @user.books.find(params[:id])
   end
-  
+
   def create
-    user = User.find(params[:id])
-    book = current_user.books.new(user_id: user.id)
-    book.user_id = current_user.id
-    if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       redirect_to book_path
     else
-      redirect_to books_path
+      render :new
     end
   end
 
   def destroy
-    book = Book.find(params[:id])
+    @user = User.find(params[:id])
+    book = @user.books.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path(@user)
   end
-  
-  
+
+  def update
+
+  end
+
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :introduction)
   end
 end
+
