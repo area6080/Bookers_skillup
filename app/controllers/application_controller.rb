41b29ea4157bd5583a85fc53e483911d1cbcf872
area6080 <161906317+area6080@users.noreply.github.com>
2024-04-12 01:31:51 +0000

    [Create]Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    super(resource)
     user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
+    super(resource)
     root_path
   end
- 
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed up successfully."
+    super(resource)
+    user_path(current_user.id)
+  end
+
 
   protected
 
@@ -18,5 +28,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-  
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,20 +11,20 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @book =Book.find(params[:id])
+    @user =User.find(@book.user_id)
   end
-
+  
   def edit
-    @user =User.find(params[:id])
     @book =Book.find(params[:id])
+    @user =User.find(@book.user_id)
   end
 
   def create
-    @book = Book.new
-    # (book_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -32,20 +32,25 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path(@user.id)
+    redirect_to books_path
   end
 
   def update
-
+    @book = Book.find(params[:id])
+    @book.update(update_book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
   end
-
 
   private
 
   def book_params
+    params.require(:book).permit(:title, :body,:user_id)
+  end
+  
+  def update_book_params
     params.require(:book).permit(:title, :body)
   end
 end
